6cd9d6442522dadff8b9b0b3fae922c364c3f096
chesyaaa <nurchesya3@gmail.com>
2025-05-28 09:47:58 +0700

    update

---
+++ app/Models/Siswa.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    protected $fillable = [
+        'nama', 'nis', 'gender', 'alamat', 'kontak', 'email', 'status_lapor_pkl', 'foto'
+    ];
+
+    //function relasi antara tabel guru dengan tabel pkls one to many
+    public function pkls()
+    {
+        return $this->hasMany(Pkl::class);
+    }
+}
